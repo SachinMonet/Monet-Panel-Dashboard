b592448814913df7fd4c4b9fb7136d8874cf7ff8
SachinMonet <sachinsharma.ashmar.in>
2026-02-16 17:14:59 +0530

    remove not use code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,46 +2,6 @@ import { Routes } from '@angular/router';
 import { Screening } from './components/screening/screening';
 
 
-// export const routes: Routes = [
-//     { path: '', redirectTo: '/login', pathMatch: 'full' },
-//     {
-//         path: 'login',
-//         loadComponent: () => import('./components/login/login').then(c => c.Login)
-//     },
-//     {
-//         path: 'screening',
-//         loadComponent: () => import('./components/screening/screening').then(c => c.Screening)
-//     },
-//     {
-//         path: 'dashboard',
-//         loadComponent: () => import('./components/dashboard/dashboard').then(c => c.Dashboard)
-//     },
-//     {
-//         path: 'providers',
-//         loadComponent: () => import('./components/providers/providers').then(c => c.Providers)
-//     },
-//     {
-//         path: 'campaigns',
-//         loadComponent: () => import('./components/campaigns/campaigns').then(c => c.Campaigns)
-//     },
-//     {
-//         path: 'monitoring',
-//         loadComponent: () => import('./components/monitoring/monitoring').then(c => c.Monitoring)
-//     },
-//     {
-//         path: 'reports',
-//         loadComponent: () => import('./components/reports/reports').then(c => c.Reports)
-//     },
-//     {
-//         path: 'settings',
-//         loadComponent: () => import('./components/settings/settings').then(c => c.Settings)
-//     },
-//     {
-//         path: '**',
-//         loadComponent: () => import('./components/not-found/not-found').then(c => c.NotFound)
-//     }
-
-// ];
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 
@@ -53,13 +13,9 @@ export const routes: Routes = [
 
   {
     path: '',
-    component: Screening,           // parent with sidebar
+    component: Screening,
     children: [
-      {
-        path: 'screening',
-        loadComponent: () =>
-          import('./components/screening/screening').then(c => c.Screening),
-      },
+
       {
         path: 'dashboard',
         loadComponent: () =>
@@ -71,27 +27,7 @@ export const routes: Routes = [
           import('./components/providers/providers').then(c => c.Providers),
       },
       {
-        path: 'campaigns',
-        loadComponent: () =>
-          import('./components/campaigns/campaigns').then(c => c.Campaigns),
-      },
-      {
-        path: 'monitoring',
-        loadComponent: () =>
-          import('./components/monitoring/monitoring').then(c => c.Monitoring),
-      },
-      {
-        path: 'reports',
-        loadComponent: () =>
-          import('./components/reports/reports').then(c => c.Reports),
-      },
-      {
-        path: 'settings',
-        loadComponent: () =>
-          import('./components/settings/settings').then(c => c.Settings),
-      },
-      {
-        path:'create-campaigns',
+        path: 'create-campaigns',
         loadComponent: () =>
           import('./components/create-campaigns/create-campaigns').then(c => c.CreateCampaigns),
       }
